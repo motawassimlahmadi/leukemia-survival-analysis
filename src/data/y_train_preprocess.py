424c6fc59f3924bac26bfe4f269c66354ba65a10
Motawassim <motawassimlahmadi@outlook.com>
2025-05-17 20:10:55 +0200

    concordance_index_censored (0.7)

--- src/data/y_train_preprocess.py
+++ src/data/y_train_preprocess.py
@@ -5,6 +5,7 @@ import seaborn as sns
 from src.data.basic_preprocess import *
 import category_encoders as ce
 from sklearn.ensemble import RandomForestRegressor
+from sksurv.util import Surv
 
 
 
@@ -21,7 +22,25 @@ def y_train_preprocess(df):
         pl.col("OS_STATUS").fill_null(strategy="backward")
     )
     
+    
+    
     return df
+
+def y_train_surv(df):
+    df = df.with_columns(
+        pl.col("OS_STATUS").cast(pl.Boolean).alias("OS_STATUS")
+    )
+    
+    df = df.rename(
+        {"OS_YEARS" : "time" ,
+        "OS_STATUS" : "event"}
+    )
+    
+    y_train_pd = df.to_pandas()
+
+    y_train_pd = Surv.from_dataframe('event' , 'time' , y_train_pd)
+    
+    return y_train_pd
 
 
 
