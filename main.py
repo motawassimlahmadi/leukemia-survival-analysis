bbe55b069e8182f3a8b2789892b5c63a66ffe2dd
Motawassim <motawassimlahmadi@outlook.com>
2025-05-17 20:10:55 +0200

    concordance_index_censored (0.7)

--- main.py
+++ main.py
@@ -6,14 +6,17 @@ from time import gmtime, strftime
 from src.data.molecular_preprocess import process_molecular_data
 from src.data.load_data import load_dataset
 from src.data.clinical_preprocess import process_clinical_data
-from src.data.y_train_preprocess import y_train_preprocess
+from src.data.y_train_preprocess import y_train_preprocess , y_train_surv
 import polars as pl
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.metrics import accuracy_score, root_mean_squared_error
 from sklearn.model_selection import train_test_split
 from sksurv.linear_model import CoxPHSurvivalAnalysis
 from sksurv.metrics import concordance_index_ipcw
+from sksurv.metrics import concordance_index_censored
 from sksurv.util import Surv
+from sksurv.ensemble import GradientBoostingSurvivalAnalysis
+import matplotlib.pyplot as plt
 
 def main():
     
@@ -46,17 +49,12 @@ def main():
     df= load_dataset(args.dataset_path)
     df = process_molecular_data(df)
     
-    for col in df.columns:
-        if df.select(pl.col(col).is_null().sum())[0, 0] > 0:
-            print(f"Nom de colonne avec valeurs nulles : {col} , Nombre : {df.select(pl.col(col).is_null().sum())[0, 0]}")
+    
     
     # Clinical data set
     cl_df = load_dataset("data/raw/X_train/clinical_train.csv")
     cl_df = process_clinical_data(cl_df)
     
-    for col in cl_df.columns:
-        if cl_df.select(pl.col(col).is_null().sum())[0, 0] > 0:
-            print(f"Nom de colonne avec valeurs nulles : {col} , Nombre : {cl_df.select(pl.col(col).is_null().sum())[0, 0]}")
     
     
     # Joining the two datasets
@@ -76,33 +74,45 @@ def main():
     
     df_pd = final_df.to_pandas()
     
+    y_feature = final_df.select("OS_STATUS","OS_YEARS")
+    X = df_pd.drop(["OS_STATUS" , "OS_YEARS"] , axis=1)
+    y = y_train_surv(y_feature)
     
-    for col in final_df.columns:
-        if final_df.select(pl.col(col).is_null().sum())[0, 0] > 0:
-            print(f"Nom de colonne avec valeurs nulles : {col} , Nombre : {final_df.select(pl.col(col).is_null().sum())[0, 0]}")
-
-
-    # Variables d'entrée
-    X = df_pd.drop(columns=["OS_STATUS", "OS_YEARS"])
-    y_class = df_pd["OS_STATUS"]
-    y_reg = df_pd["OS_YEARS"]
-
-    # Split
-    X_train, X_test, y_class_train, y_class_test, y_reg_train, y_reg_test = train_test_split(
-        X, y_class, y_reg, test_size=0.2, random_state=42
-    )
     
-    clf = RandomForestClassifier(random_state=42)
-    clf.fit(X_train, y_class_train)
-    y_class_pred = clf.predict(X_test)
-    acc = accuracy_score(y_class_test, y_class_pred)
-    print(f"Accuracy OS_STATUS: {acc:.4f}")
+    X_train , X_test , y_train_split , y_test_split = train_test_split(X , y, random_state=42, test_size=0.2)
     
-    reg = RandomForestRegressor(random_state=42)
-    reg.fit(X_train, y_reg_train)
-    y_reg_pred = reg.predict(X_test)
-    rmse = root_mean_squared_error(y_reg_test, y_reg_pred)
-    print(f"RMSE OS_YEARS: {rmse:.4f}")
+    model = CoxPHSurvivalAnalysis()
+    model.fit(X_train, y_train_split)
+    
+    risk_scores = model.predict(X_test)
+    pred_test = -model.predict(X_test)
+    
+    cindex = concordance_index_censored(y_test_split["event"], y_test_split["time"], risk_scores)
+    
+    print("Concordance Index:", cindex[0])
+    
+    
+    # # Variables d'entrée
+    # X = df_pd.drop(columns=["OS_STATUS", "OS_YEARS"])
+    # y_class = df_pd["OS_STATUS"]
+    # y_reg = df_pd["OS_YEARS"]
+
+    # # Split
+    # X_train, X_test, y_class_train, y_class_test, y_reg_train, y_reg_test = train_test_split(
+    #     X, y_class, y_reg, test_size=0.2, random_state=42
+    # )
+    
+    # clf = RandomForestClassifier(random_state=42)
+    # clf.fit(X_train, y_class_train)
+    # y_class_pred = clf.predict(X_test)
+    # acc = accuracy_score(y_class_test, y_class_pred)
+    # print(f"Accuracy OS_STATUS: {acc:.4f}")
+    
+    # reg = RandomForestRegressor(random_state=42)
+    # reg.fit(X_train, y_reg_train)
+    # y_reg_pred = reg.predict(X_test)
+    # rmse = root_mean_squared_error(y_reg_test, y_reg_pred)
+    # print(f"RMSE OS_YEARS: {rmse:.4f}")
     
     
     
